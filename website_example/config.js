6eaa5d17b6faa293040f61738660813db2018eb6
Xpunk7 <104718343+xpunk7@users.noreply.github.com>
2022-05-14 09:26:23 -0400

    Create config.js

---
+++ website_example/config.js
@@ -0,0 +1,16 @@
+var api = "https://pool.exohood.com/api";
+
+var coinUnits = 100;
+var coinDecimalPlaces = 2;
+
+var poolHost = "pool.exohood.com";
+
+var irc = "irc.freenode.net/#monero-pools";
+
+var email = "support@exohood.com";
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://blockexplorer.exohood.com/block/";
+
+var transactionExplorer = "https://blockexplorer.exohood.com/tx/";
